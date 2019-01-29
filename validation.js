6c1168d090da754d256141c95d1ca0f387b61753
ishrat03 <31351750+ishrat03@users.noreply.github.com>
2019-01-29 12:34:49 +0530

    29.
    29 date

---
+++ validation.js
@@ -0,0 +1,7 @@
+function validation(){
+	var status="true";
+	var name=document.getElementById('name').value;
+	if (name=="ishrat") {
+		document.getElementById('nameerror').style.display="inline";
+	}
+}
